261120a5d37d9f96dfb751b74b4b83672fa8c918
Tobias Lundin <tobias.lundin@gmail.com>
2017-08-14 20:52:19 +0200

    use request package to get rss feeds via server to get around cors issues

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
+const request = require('request');
 const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
 
+app.get('/rss/:url', (req, res) => {
+  // pipe request to response
+  console.log(`piping response via "request" to ${req.params.url}`);
+  var xRequest = request(req.params.url);
+  req.pipe(xRequest);
+  xRequest.pipe(res);
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
-
