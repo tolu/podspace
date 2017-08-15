62e630f5a7b74e1a3b1c4ca7b262ad414b394e98
Tobias Lundin <tobias.lundin@gmail.com>
2017-08-15 06:51:48 +0200

    move rss fetching to module

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 import audioPlayer from './audioPlayer.js';
 import searchResultComponent from './components/searchResultList.js';
 import showComponent from './components/show.js';
+import rssFetcher from './rssFetcher.js';
 
 // https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
 const SEARCH_BASE = '//itunes.apple.com/search?media=podcast&entity=podcast&limit=25&term=';
@@ -67,19 +68,11 @@ document.addEventListener('click', (event) => {
   }
 });
 
-const domParser = new DOMParser();
-const parseXml = (xml) => domParser.parseFromString(xml, 'application/xml');
 function displayShow(rssFeed, el) {
   console.info('display', rssFeed);
   const showImageUrl = el.querySelector('img').src || '';
   el.classList.add('loading');
-  fetch(`/rss/${encodeURIComponent(rssFeed)}`)
-    .then(res => res.text())
-    .then((rss) => {
-      const xml = parseXml(rss);
-      const json = xmlToJson(xml);
-      return json.rss.channel;
-    })
+  rssFetcher(`/rss/${encodeURIComponent(rssFeed)}`)
     .then((show) => {
       el.classList.remove('loading');
       const showList = document.querySelector('.show-list');
@@ -87,42 +80,4 @@ function displayShow(rssFeed, el) {
     });
 }
 
-function xmlToJson(xml) {
-  // based on: https://davidwalsh.name/convert-xml-json
-  // Create the return object
-	var obj = {};
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
-			for (var j = 0; j < xml.attributes.length; j++) {
-				var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-  }
-  
-	var nrOfChildren = xml.children.length;
-	if (!nrOfChildren && xml.textContent.length) {
-	    return xml.textContent;
-	} else { // do children
-		for(var i = 0; i < nrOfChildren; i++) {
-			var item = xml.children.item(i);
-      var nodeName = item.nodeName;
-      if(/:/i.test(nodeName)){ // skip namespaced elements
-        continue;
-      }
-			if (typeof(obj[nodeName]) == "undefined") {
-				obj[nodeName] = xmlToJson(item);
-			} else {
-				if (typeof(obj[nodeName].push) == "undefined") {
-					var old = obj[nodeName];
-					obj[nodeName] = [];
-          obj[nodeName].push(old);
-				}
-				obj[nodeName].push(xmlToJson(item));
-			}
-		}
-	}
-	return obj;
-};
+
---
+++ public/js/rssFetcher.js
@@ -0,0 +1,53 @@
+
+const domParser = new DOMParser();
+const parseXml = (xml) => domParser.parseFromString(xml, 'application/xml');
+
+export default (rssFeedUrl) => {
+  return fetch(rssFeedUrl)
+  .then(res => res.text())
+  .then((rss) => {
+    const xml = parseXml(rss);
+    const json = xmlToJson(xml);
+    return json.rss.channel;
+  });
+}
+
+function xmlToJson(xml) {
+  // based on: https://davidwalsh.name/convert-xml-json
+  // Create the return object
+	var obj = {};
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+  }
+  
+	var nrOfChildren = xml.children.length;
+	if (!nrOfChildren && xml.textContent.length) {
+	    return xml.textContent;
+	} else { // do children
+		for(var i = 0; i < nrOfChildren; i++) {
+			var item = xml.children.item(i);
+      var nodeName = item.nodeName;
+      if(/:/i.test(nodeName)){ // skip namespaced elements
+        continue;
+      }
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+          obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
