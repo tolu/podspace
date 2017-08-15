466fff88449e74aa7d5fb1091415faad95cc8a7b
Tobias Lundin <tobias.lundin@gmail.com>
2017-08-15 08:06:01 +0200

    add container around content and add modal for messages

---
+++ public/js/components/modal.js
@@ -0,0 +1,12 @@
+
+const modal = document.querySelector('.modal');
+export const displayMessage = (message, close) => {
+  const messageEl = modal.querySelector('.modal__message');
+  messageEl.innerHTML = message;
+  modal.style.display = 'block';
+}
+
+export const hideMessage = () => {
+  displayMessage('');
+  modal.style.display = 'none';
+}
