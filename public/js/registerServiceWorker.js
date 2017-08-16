dd654910ca17f27e834d62ada6d9a3a17768d173
Tobias Lundin <tobias.lundin@gmail.com>
2017-08-16 12:34:05 +0200

    more relative links please

--- public/js/registerServiceWorker.js
+++ public/js/registerServiceWorker.js
@@ -3,7 +3,7 @@
 export default () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(registration => {
+      navigator.serviceWorker.register('../sw.js').then(registration => {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, err => {
