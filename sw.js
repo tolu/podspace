0842c5e384a8db64ce3d35c01b94f227e53408f0
Tobias Lundin <tobias.lundin@gmail.com>
2017-08-19 12:10:18 +0200

    only try network in sw if we are online remove some logs

--- sw.js
+++ sw.js
@@ -13,6 +13,9 @@ const appShellFiles = [
 const log = (...args) => {
   console.debug('%c SW ', 'background: #222; color: #bada55', ...args);
 }
+const info = (...args) => {
+  console.info('%c SW ', 'background: #222; color: #bada55', ...args);
+}
 
 // add timeout-method to fetch
 Promise.prototype.timeout = function(number){
@@ -24,7 +27,7 @@ Promise.prototype.timeout = function(number){
 
 // INSTALL
 self.addEventListener('install', event => {
-  log('SW install...');
+  info('SW install...');
   caches.delete(FILE_CACHE);
   cacheAppShell(event);
 });
@@ -34,7 +37,7 @@ function cacheAppShell(event) {
   event.waitUntil(
     caches.open(FILE_CACHE)
       .then(cache => cache.addAll(appShellFiles))
-      .then(_ => log('File cache completed...'))
+      .then(_ => info('File cache completed...'))
       .then(_ => self.skipWaiting())
   );
 }
@@ -43,13 +46,20 @@ function cacheAppShell(event) {
 self.addEventListener('fetch', event => {
   const { request } = event;
   const path = (new URL(request.url)).pathname;
-  if(request.method !== 'GET'){
+  if(request.method !== 'GET') {
+    return;
+  }
+  if(/\/token/.test(request.url)) {
     return;
   }
 
   // use network first strategy for application files
   if(appShellFiles.includes(path)){
-    return event.respondWith(networkFirstAndCacheForSlow(request, FILE_CACHE));
+    if(navigator.onLine){
+      return event.respondWith(networkFirstAndCacheForSlow(request, FILE_CACHE));
+    } else {
+      return event.respondWith(cacheFirst(request, FILE_CACHE));
+    }
   }
 
   // select cache based on url
@@ -63,8 +73,7 @@ self.addEventListener('fetch', event => {
   // event.respondWith(networkFirstAndCacheForSlow(req));
 });
 
-function fetchNcache(req, cacheKey){
-  log('fetch n cache', req.url);
+function fetchNcache(req, cacheKey) {
   // IMPORTANT: Clone the request. A request is a stream and
   // can only be consumed once. Since we are consuming this
   // once by cache and once by the browser for fetch, we need
@@ -72,18 +81,19 @@ function fetchNcache(req, cacheKey){
   const reqClone = req.clone();
   return fetch(reqClone).then(res => {
     // Check if we received a valid response
-    if(!res || res.status >= 400) {
-      log('res is invalid for', req.url);
-      return res;
+    if(!res.ok) {
+      const {type, status} = res;
+      info('res is not ok!', req.url, {type, status});
     }
     // IMPORTANT: Clone the response. A response is a stream
     // and because we want the browser to consume the response
     // as well as the cache consuming the response, we need
     // to clone it so we have two streams.
-    log('store res in cache for', req.url);
+    const path = `/${req.url.split('/').slice(3).join('/')}`;
     const resClone = res.clone();
     caches.open(cacheKey)
-      .then(cache => cache.put(req, resClone));
+      .then(cache => cache.put(req, resClone))
+      .then(() => info(`Cached ${path}`));
     return res;
   });
 }
@@ -102,10 +112,9 @@ function cacheFirst(req, cacheKey){
     .then((response) => {
       if (response) {
         // Cache hit - return response
-        log('Cache hit');
+        info('Cache hit');
         return response;
       }
-      log('Cache miss');
       return fetchNcache(req, cacheKey);
     })
 }
